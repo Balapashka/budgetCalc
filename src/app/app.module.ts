a4fad27803f9ac266438fdd322913e90377ea9cb
balapashka <eldos.sergali@mail.ru>
2023-08-23 18:36:14 +0600

    Verstka

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { AddItemFormComponent } from './components/add-item-form/add-item-form.component';
+import { BudgetItemListComponent } from './components/budget-item-list/budget-item-list.component';
+import { BudgetItemCardComponent } from './components/budget-item-list/budget-item-card/budget-item-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    AddItemFormComponent,
+    BudgetItemListComponent,
+    BudgetItemCardComponent
   ],
   imports: [
     BrowserModule,
